103e796f7cdeb876d310fd75caeedbbb5b18fea5
Kevin Grace <Kevin@kevins-air.home>
2021-09-18 13:17:54 -0400

    added connections removed schema

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({force: false}).then(() => {
-  app.listen(PORT, () => console.log('App now listening'));
+  app.listen(PORT, () => console.log('App now listening using PORT ${PORT}!'));
 });
 
 
